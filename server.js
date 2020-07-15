6c753b93cf8f60aea599f11b667feb39b7e033e0
alex <apozohue10@gmail.com>
2020-07-15 12:05:50 +0200

    a11y

--- server.js
+++ server.js
@@ -407,23 +407,23 @@ function request_ap_and_reencrypt(json, response_to, personIdentifier, needed_at
                     console.log("VUELTA --> RAP&REEN: AP me devuelve ", response);
 
                     // var address_xml;
-                    if (response.HomeInstitutionAddress) {
-                         var address_values = response.HomeInstitutionAddress.split(' '); 
-                         var local_designator = '<eidas:LocatorDesignator>'+address_values[2]+'</eidas:LocatorDesignator>'; 
-                         var thoroughfare = '<eidas:Thoroughfare>'+address_values[0] + ' ' + address_values[1]+'</eidas:Thoroughfare>';
-                         var post_name = '<eidas:PostName>'+address_values[4]+'</eidas:PostName>';
-                         var post_code = '<eidas:PostCode>'+address_values[3]+'</eidas:Postcode>';
-                         address_xml = local_designator + thoroughfare + post_name + post_code;
-                         address_xml = new Buffer(address_xml).toString('base64');
-                         response.HomeInstitutionAddress = address_xml;
-                    }
+                    // if (response.HomeInstitutionAddress) {
+                    //      var address_values = response.HomeInstitutionAddress.split(' '); 
+                    //      var local_designator = '<eidas:LocatorDesignator>'+address_values[2]+'</eidas:LocatorDesignator>'; 
+                    //      var thoroughfare = '<eidas:Thoroughfare>'+address_values[0] + ' ' + address_values[1]+'</eidas:Thoroughfare>';
+                    //      var post_name = '<eidas:PostName>'+address_values[4]+'</eidas:PostName>';
+                    //      var post_code = '<eidas:PostCode>'+address_values[3]+'</eidas:Postcode>';
+                    //      address_xml = local_designator + thoroughfare + post_name + post_code;
+                    //      address_xml = new Buffer(address_xml).toString('base64');
+                    //      response.HomeInstitutionAddress = address_xml;
+                    // }
 
 
-                    if (needed_attributes.includes("CurrentPhoto") && personIdentifier === '99999142H') {
-                        var image_data = new Buffer(fs.readFileSync('img/child.jpg')).toString('base64');
-                        var image = '<eid4u:document xmlns:eid4u="http://eidas.europa.eu/attributes/sectorspecific/eid4u" name="child.jpg" type="photo" contentType="image/jpeg">'+ image_data +'</eid4u:document>'
-                        response.CurrentPhoto = new Buffer(image).toString('base64');
-                    }
+                    // if (needed_attributes.includes("CurrentPhoto") && personIdentifier === '99999142H') {
+                    //     var image_data = new Buffer(fs.readFileSync('img/child.jpg')).toString('base64');
+                    //     var image = '<eid4u:document xmlns:eid4u="http://eidas.europa.eu/attributes/sectorspecific/eid4u" name="child.jpg" type="photo" contentType="image/jpeg">'+ image_data +'</eid4u:document>'
+                    //     response.CurrentPhoto = new Buffer(image).toString('base64');
+                    // }
 
                     /////// TODO: ESTOY HAY QUE VER PORQUE NO SE DEBEN PEDIR SIEMPRE ESTOS
                     // response = {
